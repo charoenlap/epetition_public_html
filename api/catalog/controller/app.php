41c0a8fbc3282a51bf070cbd0e1c03a6ddf9d538
charoenlap <charoenlap88@gmail.com>
2022-03-15 17:29:16 +0700

    update

--- api/catalog/controller/app.php
+++ api/catalog/controller/app.php
@@ -26,25 +26,27 @@
 			$result = $result = array(
 				'code'		=>200,
 				'status' 	=> 'failed',
-				'desc' 		=> ''
+				'desc' 		=> 'topic id empty'
 			);
 			if(method_post()){
 				$post 				= $_POST;
-				$post['topic_id'] 	= post('topic_id');
-				// unset($post['file-upload-field']);
-				// if(isset($_FILES['file-upload-field'])){
-				// 	$upload_name = time().$_FILES['file-upload-field']['name'];
-				// 	upload($_FILES['file-upload-field'],'uploads/files/',$upload_name);
-				// 	$post['file'] = $upload_name;
-				// }
-				// exit();
-				$add = $master->addResponse($post);
-				
-				$result = array(
-					'code'		=> 200,
-					'status' 	=> 'success',
-					'desc' 		=> $data
-				);
+				if(isset($post['topic_id'])){
+					$post['topic_id'] 	= post('topic_id');
+					// unset($post['file-upload-field']);
+					// if(isset($_FILES['file-upload-field'])){
+					// 	$upload_name = time().$_FILES['file-upload-field']['name'];
+					// 	upload($_FILES['file-upload-field'],'uploads/files/',$upload_name);
+					// 	$post['file'] = $upload_name;
+					// }
+					// exit();
+					$add = $this->model('master')->addResponse($post);
+
+					$result = array(
+						'code'		=> 200,
+						'status' 	=> 'success',
+						'desc' 		=> $data
+					);
+				}
 			}
 			
 			$this->json($result);
