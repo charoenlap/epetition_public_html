8bcaf448acced51e9d0d2022e6ad087ecf902a60
charoenlap <charoenlap88@gmail.com>
2022-03-15 16:40:45 +0700

    update

--- api/catalog/controller/home.php
+++ api/catalog/controller/home.php
@@ -1,53 +1,49 @@
 <?php 
 	class HomeController extends Controller {
-	    public function index() {
-	    	echo "test";
-	    }
-	    public function getToken(){
-	    	$data = array();
-	    	$input = array(
-	    		'func' => 'checkTicket',
-	    		'username' => '',
-	    		'password' => ''
-	    	);
-	    	$result = array(
-	    		'result' => 'success',
-	    		'desc'	=> 'get token',
-	    		'code'	=> 200,
-	    		'token' => encrypt(md5('token'))
-	    	);
-	    	$this->json($result);
-	    }
-	    public function getTicket(){
-	    	$data = array();
-	    	$input = array(
-	    		'ticket_id' => '1',
-	    		'token'	=>	'aVF1c3dLMGdxaVhEMUpCdHludVltcUVndHVrOWd5K2VocWppR0NEb2NFT1hlcEZxWktwVDdxMUlDKzFnR3NMZw=='
-	    	);
-	    	$result_ticket = $this->model('master')->getTicket($input);
-	    	$result = array(
-	    		'result' => 'success',
-	    		'desc'	=> 'get ticket',
-	    		'code'	=> 200,
-	    		'detail' => $result_ticket
-	    	);
-	    	$this->json($result);
-	    }
-	    public function getTickets(){
-	    	$data = array();
-	    	// $user_id = 
-	    	$input = array(
-	    		'token'	=>	'aVF1c3dLMGdxaVhEMUpCdHludVltcUVndHVrOWd5K2VocWppR0NEb2NFT1hlcEZxWktwVDdxMUlDKzFnR3NMZw=='
-	    	);
-
-	    	$result_ticket = $this->model('master')->getTickets($input);
-	    	$result = array(
-	    		'result' => 'success',
-	    		'desc'	=> 'get tickets',
-	    		'code'	=> 200,
-	    		'detail' => $result_ticket
-	    	);
-	    	$this->json($result);
-	    }
+		public function getToken(){
+			$result = array();
+			$result = array(
+				'code'		=>200,
+				'status' 	=> 'failed',
+				'desc' 		=> ''
+			);
+			$this->json($result);
+		}
+		public function getCases(){
+			$result = array();
+			$result = array(
+				'code'		=>200,
+				'status' 	=> 'failed',
+				'desc' 		=> ''
+			);
+			$this->json($result);
+		}
+		public function getCase(){
+			$result = array();
+			$result = array(
+				'code'		=>200,
+				'status' 	=> 'failed',
+				'desc' 		=> ''
+			);
+			$this->json($result);
+		}
+		public function getOperation(){
+			$result = array();
+			$result = array(
+				'code'		=>200,
+				'status' 	=> 'failed',
+				'desc' 		=> ''
+			);
+			$this->json($result);
+		}
+		public function sendCase(){
+			$result = array();
+			$result = array(
+				'code'		=>200,
+				'status' 	=> 'failed',
+				'desc' 		=> ''
+			);
+			$this->json($result);
+		}
 	}
 ?>
