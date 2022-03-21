4661b5bb3806da317b851a9979b73181219f4b61
charoenlap <charoenlap88@gmail.com>
2022-03-21 17:12:52 +0700

    update

--- admin_back_office/openid/SSOLogin.php
+++ admin_back_office/openid/SSOLogin.php
@@ -8,8 +8,10 @@ ob_start();
 session_start();
 session_destroy();
 // สรอ. OpenID Provider
-// $OpenIdProviderUrl = 'https://testopenid2.ega.or.th';  
-$OpenIdProviderUrl = 'https://www.google.co.th';  
+
+$OpenIdProviderUrl = 'https://testopenid2.ega.or.th';  
+// $OpenIdProviderUrl = 'https://govid.egov.go.th';  
+
 //หน้าที่จะให้ส่งกลับ
 // $eServiceUrl = "http://127.0.0.1:8090/OpenID/"; 
 // $eServiceUrl = "https://e-petition.energy.go.th/openid/"; 
