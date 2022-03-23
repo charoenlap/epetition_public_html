25b3d76cb34979e265d5b7dbee8f982c47f2514b
charoenlap <charoenlap88@gmail.com>
2022-03-23 08:26:35 +0700

    update

--- catalog/view/theme/form.php
+++ catalog/view/theme/form.php
@@ -338,7 +338,6 @@
     </div>
 </section>
 
-<?php } ?>
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
